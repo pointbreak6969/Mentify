30ebe3dd4a94b33fd1bb899da625f9e7012591df
BishowDevkota <116930015+BishowDevkota@users.noreply.github.com>
2024-06-15 09:16:30 +0545

    style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
-
+import Nav from "./components/Nav/Nav";
 
 function App() {
- 
-
   return (
     <>
-   
+      <h1 class="text-4xl underline">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
