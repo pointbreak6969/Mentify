917c43f78a851844a5d802f429b27d07adfa97e6
Biraj Baral <117191235+pointbreak6969@users.noreply.github.com>
2024-06-15 08:44:40 +0545

    let's rock hackathon

---
+++ client/src/context/context.js
@@ -0,0 +1,13 @@
+import React, {createContext, useState} from 'react'
+
+const AppContext = createContext();
+
+const AppProvider = ({children}) => {
+  return (
+  <AppContext.Provider  >
+    {children}
+  </AppContext.Provider>
+  )
+}
+
+export {AppProvider, AppContext}
