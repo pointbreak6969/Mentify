1db0b6e46564a044e42ceb97583dcdab07346b33
Biraj Baral <117191235+pointbreak6969@users.noreply.github.com>
2024-06-15 23:01:21 +0545

    style

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,32 +1,29 @@
-import React from 'react'
-import "./Card.css"
+import React from "react";
+import "./Card.css";
 const Card = () => {
-    const cards=[
-        {
-            quote:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti accusantium aperiam possimus natus rerum dolor soluta, esse officia maiores ratione.",
-            writer:"MArk Davidson"
-        },
-        {
-          quote:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti accusantium aperiam possimus natus rerum dolor soluta, esse officia maiores ratione.",
-          writer:"Richard Davidson"
-      }
-    ]
+  const cards = [
+    {
+      quote:
+        "Lorem ipsum dofsjdklafjskldlor, sit amet consectetur adipisicing elit. Deleniti accusantium aperiam possimus natus rerum dolor soluta, esse officia maiores ratione.",
+      writer: "MArk Davidson",
+    },
+    {
+      quote:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti accusantium aperiam possimus natus rerum dolor soluta, esse officia maiores ratione.",
+      writer: "Richard Davidson",
+    },
+  ];
   return (
-    <div className='card-wrapper'>
-      {
-cards.map((item,index)=>{
-return(
-    <div className='card' key={index}>
-      <div className="content">
-      {item.quote}
-      </div>
-      <div className="writer">
-      {item.writer}
-      </div>
+    <div className="card-wrapper">
+      {cards.map((item, index) => {
+        return (
+          <div className="card" key={index}>
+            <div className="content">{item.quote}</div>
+            <div className="writer">{item.writer}</div>
+          </div>
+        );
+      })}
     </div>
-)
-})}
-    </div>
-  )
-}
-export default Card
+  );
+};
+export default Card;
